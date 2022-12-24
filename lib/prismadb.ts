8e1801dc3ba38ce369fe3de25ed89d535818e54d
TriDvaRas <02dimon02@gmail.com>
2022-12-24 22:53:00 +0200

    init

---
+++ lib/prismadb.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client"
+
+declare global {
+    var _prisma: PrismaClient | undefined
+}
+
+const prisma = globalThis._prisma || new PrismaClient()
+if (process.env.NODE_ENV !== "production") globalThis._prisma = prisma
+
+export default prisma
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import DiscordProvider from "next-auth/providers/discord"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "../../../lib/prismadb"
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        DiscordProvider({
+            clientId: process.env.DISCORD_CLIENT_ID as string,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET as string
+        })
+    ],
+}
+export default NextAuth(authOptions)
